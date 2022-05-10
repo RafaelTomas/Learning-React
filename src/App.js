a38e0e00ef1de5f6c93ce673f5cb6466ee0ac1cf
Rafael Tomás <rafaeltomassouza@gmail.com>
2022-05-10 11:11:13 -0300

    feat: useState

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [reverse, setReverse] = useState(false);
+  const [count, setCount] = useState(0);
+  const reverseClass = reverse ? 'reverse' : '';
+
+  const handleClick = () => {
+    setReverse(!reverse);
+    setCount(count + 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
+        <h1>Click in button: {count} </h1>
+        <button onClick={handleClick} type="button">
+          Reverse
+        </button>
       </header>
     </div>
   );
