e6a19957cc44ea87b074e81d5defafc8fdaf9ead
Rafael Tomas <rafaeltomassouza@gmail.com>
2022-05-19 15:53:11 -0300

    useContext-parte-1

--- src/App.js
+++ src/App.js
@@ -1,64 +1,24 @@
-import P from 'prop-types';
-import { useEffect, useMemo, useState, useRef } from 'react';
+import React, { useContext, useState } from 'react';
 import './App.css';
 
-const Post = ({ post, handleClick }) => {
-  return (
-    <div key={post.id} className="post">
-      <h1 onClick={() => handleClick(post.title)}>{post.title}</h1>
-      <p>{post.body}</p>
-    </div>
-  );
+//Estado inicial..
+const globalState = {
+  title: 'Rafael Tomás',
+  body: ' o coro',
+  counter: 0,
 };
 
+//Criação de um contexto, e um componente React pq precisa chamar um Provider
+const GlobalContext = React.createContext();
+
+//Esse value ele vai passar esse valor para todos os filhos dos componentes
 function App() {
-  const [posts, setPosts] = useState([]);
-  const [value, setValue] = useState('');
-  const input = useRef(null);
-
-  console.log('foi renderizado!!');
-
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then((res) => res.json())
-      .then((res) => setPosts(res));
-  });
-
-  useEffect(() => {
-    input.current.focus();
-  }, [value]);
-
-  const handleClick = (value) => {
-    setValue(value);
-  };
-
+  const [contextState, setContextState] = useState(globalState);
   return (
-    <div className="App">
-      <input
-        ref={input}
-        type="search"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-      />
-      {useMemo(() => {
-        return (
-          posts.length > 0 &&
-          posts.map((post) => (
-            <Post key={post.id} post={post} handleClick={handleClick} />
-          ))
-        );
-      }, [posts])}
-    </div>
+    <GlobalContext.Provider value={{ contextState, setContextState }}>
+      <Div />
+    </GlobalContext.Provider>
   );
 }
 
 export default App;
-
-Post.propTypes = {
-  post: P.shape({
-    id: P.number,
-    title: P.string,
-    body: P.string,
-  }),
-  handleClick: P.func,
-};
