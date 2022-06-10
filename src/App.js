4644e72846ade35dae8f766c3fd7ffe7f079e42f
Rafael Tomas <rafaeltomassouza@gmail.com>
2022-06-10 07:51:41 -0300

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
+import { useReducer } from 'react';
+import { globalState } from './contexts/AppContext/data';
 import './App.css';
-import { Div } from './components/Div';
-import { AppContext } from './contexts/AppContext';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'muda': {
+      return { ...state, title: action.payload };
+    }
+    case 'inverter': {
+      const { title } = state;
+      return { ...state, title: title.split('').reverse().join('') };
+    }
+  }
+  return { ...state };
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, globalState);
+  const { couter, title, body } = state;
   return (
-    <AppContext>
-      <Div />
-    </AppContext>
+    <div>
+      <h1>
+        {title} {couter}
+      </h1>
+      {body}
+      <button
+        onClick={() =>
+          dispatch({
+            type: 'muda',
+            payload: new Date().toLocaleString('pt-Br'),
+          })
+        }
+      >
+        Click
+      </button>
+      <button onClick={() => dispatch({ type: 'inverter' })}> inverter</button>
+    </div>
   );
 }
 export default App;
