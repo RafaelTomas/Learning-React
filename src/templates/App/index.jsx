90cc63be68bf2412c3e1efd45507a4a877129205
Rafael Tomas <rafaeltomassouza@gmail.com>
2022-06-27 21:45:19 -0300

    useAsync

--- src/templates/App/index.jsx
+++ src/templates/App/index.jsx
@@ -1,19 +1,74 @@
-import './styles.css';
+import React, { useCallback, useEffect, useState } from 'react';
 
-import { PostsProvider } from '../../contexts/PostsProvider';
-import { Posts } from '../../components/Posts';
-import { CounterProvider } from '../../contexts/CounterProvider';
+const useAsync = (asyncFunction, shouldRun) => {
+  const [state, setState] = useState({
+    result: null,
+    error: null,
+    status: 'idle',
+  });
 
-function App() {
-  return (
-    <CounterProvider>
-      <PostsProvider>
-        <div>
-          <Posts />
-        </div>
-      </PostsProvider>
-    </CounterProvider>
-  );
-}
+  const run = useCallback(() => {
+    setState({
+      result: null,
+      error: null,
+      status: 'pending',
+    });
+    return asyncFunction()
+      .then((response) => {
+        setState({
+          result: response,
+          error: null,
+          status: 'settled',
+        });
+      })
+      .catch((error) => {
+        setState({
+          result: null,
+          error: error,
+          status: 'error',
+        });
+      });
+  }, [asyncFunction]);
+  useEffect(() => {
+    if (shouldRun) {
+      run();
+    }
+  }, [run, shouldRun]);
 
-export default App;
+  return [run, state.result, state.error, state.status];
+};
+
+const fetchData = async () => {
+  const data = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const json = await data.json();
+  return json;
+};
+
+export const App = () => {
+  const [reFetchData, result, error, status] = useAsync(fetchData, true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      reFetchData();
+    }, 10000);
+  }, [reFetchData]);
+
+  function handleClick() {
+    reFetchData();
+  }
+
+  if (status === 'idle') {
+    return <pre>Nada executando</pre>;
+  }
+  if (status === 'pending') {
+    return <pre>Loading</pre>;
+  }
+  if (status === 'error') {
+    return <pre> {JSON.stringify(error, null, 2)}</pre>;
+  }
+  if (status === 'settled') {
+    return <pre onClick={handleClick}> {JSON.stringify(result, null, 2)}</pre>;
+  }
+
+  return '';
+};
